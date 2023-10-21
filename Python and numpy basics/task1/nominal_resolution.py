416cfba6e9ecb5ce734cbecc3ea740b081f2496b
Svyatoslav Lipatov <92099105+svyatoslavlipatov@users.noreply.github.com>
2023-10-21 16:09:31 +0800

    Update nominal_resolution.py

--- Python and numpy basics/task1/nominal_resolution.py
+++ Python and numpy basics/task1/nominal_resolution.py
@@ -8,7 +8,12 @@ def calculate_nominal_resolution(filename):
         resolution = max_size_mm / image_data.shape[1]
         return resolution
     
-filenames = ["files/figure1.txt", "files/figure2.txt", "files/figure4.txt", "files/figure5.txt", "files/figure6.txt"]
+filenames = ["task1/files/figure1.txt", 
+             "task1/files/figure2.txt",
+             "task1/files/figure4.txt",
+             "task1/files/figure5.txt",
+             "task1/files/figure6.txt"]
+
 for filename in filenames:
     resolution = calculate_nominal_resolution(filename)
     print(f"Номинальное разрешение для {filename}: {resolution} мм/пиксель")
