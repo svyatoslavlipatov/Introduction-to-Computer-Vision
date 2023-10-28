e6ebe12e0abfda005ba30857f32132d414f95c2a
Svyatoslav Lipatov <92099105+svyatoslavlipatov@users.noreply.github.com>
2023-10-28 11:40:05 +0800

    Update coins.py

--- Python and numpy basics/task4(coins)/coins.py
+++ Python and numpy basics/task4(coins)/coins.py
@@ -5,22 +5,23 @@ from skimage.measure import label
 def area(labeled, label=1):
     return (labeled[labeled == label] / label).sum()
 
-coins = np.load("coins.npy")
+coins = np.load("Python and numpy basics/task4(coins)/coins.npy")
 labeled = label(coins)
 
 count_areas = {69:0, 145:0, 305:0, 609:0}
 nominals = [1, 2, 5, 10]
 sum_list = []
+c = 0
 
 for i in range(1, np.max(labeled) + 1):
     n = area(labeled, i)
     if n in count_areas:
         count_areas[n] += 1
-c = 0
+
 for key in count_areas:
-    enumerate(c)
     sum_of_nominal = count_areas[key] * nominals[c] 
     sum_list.append(sum_of_nominal)
+    c += 1
     
 print(f"Общая сумма: {sum(sum_list)}")
 plt.imshow(coins)
